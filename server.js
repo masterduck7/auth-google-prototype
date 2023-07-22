9b162711cd7caab5dd1145e1bff7edf6bca49eec
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-07-22 14:00:59 +0200

    chore(routes): Remove routes for security

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ require('./src/config/passport');
 
 const app = require('./src/config/app');
 const indexRoutes=require('./src/routes/index');
-const userRoutes=require('./src/routes/user');
 
 app.use("/", indexRoutes);
-app.use("/user/", userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port , () => console.log('App listening on port ' + port));
